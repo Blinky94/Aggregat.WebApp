ae12c6252dcbc22152a9842aa6492582af61e4b8
BlinkyCrazyDev <fredericcazesulfourt@yahoo.fr>
2021-08-22 17:21:33 +0200

    First commit

---
+++ ClientApp/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# ASP.NET Core Vue Vuetify and Typescript Template
+
+The repository contains an ASP.&#8203;NET Core + Vue.js Vuetify and Typescript template. The template runs on ASP.NET Core 5.0 and is created by Vue CLI 4.0 with a new plugin based architecture allowing developers to interactively scaffold a new project with just a one command.
